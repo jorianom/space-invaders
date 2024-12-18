4cbe2f0b5be36199b03c2106e0348d2d662dd92e
Riaño John <jhon091199@gmail.com>
2024-12-18 07:51:10 -0500

    first version space invaders

---
+++ class/Player.js
@@ -0,0 +1,26 @@
+class Player {
+    constructor() {
+        this.x = width / 2;
+        this.dir = 0;
+        this.score = 0;
+    }
+
+    show() {
+        fill(0, 255, 0);
+        rectMode(CENTER);
+        rect(this.x, height - 40, 40, 20);
+    }
+
+    setDir(dir) {
+        this.dir = dir;
+    }
+
+    setScore(score) {
+        this.score += score;
+    }
+
+    move() {
+        this.x += this.dir * playerSpeed;
+        this.x = constrain(this.x, 20, width - 20);
+    }
+}
